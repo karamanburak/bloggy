d1e9ce67351cc72f0f6ab66b1b261e4b0330b1c3
Burak Karaman <karaman.buraak@gmail.com>
2025-12-08 18:23:31 +0100

    style: adjust grid layout in Blogs component for improved spacing and presentation

--- src/pages/Blogs.jsx
+++ src/pages/Blogs.jsx
@@ -236,7 +236,7 @@ const Blogs = () => {
               <>
                 {currentBlogs.length > 0 ? (
                   <>
-                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mb-12">
+                    <div className="grid grid-cols-1 md:grid-cols-2 gap-8 mb-12">
                       {currentBlogs.map((blog) => (
                         <BlogCard key={blog._id} {...blog} />
                       ))}
